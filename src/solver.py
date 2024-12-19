648e419a63f045c506f1c56eee5aef8749c38347
bumjkim <bumjkim@gmail.com>
2024-12-19 21:21:01 +0900

    completed version of solver without joker

--- src/solver.py
+++ src/solver.py
@@ -9,16 +9,8 @@ TILE_COLOR_MAX = 4
 TILE_BULK_MAX = 2
 
 
-class Solver:
-    _board: Board
-    _player: Player
-
+class TileHolder:
     _tiles: [[Tile | None]] = [[None for x in range(TILE_NUM_MAX)] for y in range(TILE_COLOR_MAX * TILE_BULK_MAX)]
-    _solution = []
-
-    def __init__(self, board: Board, player: Player):
-        self._board = board
-        self._player = player
 
     def _clear_tiles(self):
         for col in range(TILE_COLOR_MAX * TILE_BULK_MAX):
@@ -47,7 +39,6 @@ class Solver:
             for depth in range(TILE_BULK_MAX):
                 if self._get_a_tile(tile.color.value + offset, tile.number) is not None:
                     offset += TILE_COLOR_MAX
-                    break
             if offset >= TILE_COLOR_MAX * TILE_BULK_MAX:
                 raise ValueError(f"There are {tile.to_print()} tile more than {TILE_BULK_MAX}")
             tile.used = False
@@ -63,18 +54,22 @@ class Solver:
         if len(solution) > 0:
             self._solution.append(copy.deepcopy(solution))
 
-    def _remove_solution_to_end(self, sets: list[Tile], solution: list):
-        try:
-            idx = solution.index(sets)
-            del solution[idx - 1:]
-        except ValueError:
-            print("Logical error.. it is not possible")
-
     def _get_a_tile(self, color: int, number: int) -> Tile | None:
         return self._tiles[color][number - 1]
 
     def _set_a_tile(self, color: int, number: int, tile: Tile | None):
         self._tiles[color][number - 1] = tile
+
+
+class Solver(TileHolder):
+    _board: Board
+    _player: Player
+
+    _solution = []
+
+    def __init__(self, board: Board, player: Player):
+        self._board = board
+        self._player = player
 
     def find_optimal_solution(self):
         self._clear_tiles()
@@ -96,7 +91,7 @@ class Solver:
                         if not tile.must:
                             print('*', end='')
                 print(' ] ', end='')
-            print ('')
+            print('')
 
     def _search_optimal(self, color: int, number: int, solution: list):
         n_col, n_num = self._find_next_avail_tile(color, number)
@@ -109,27 +104,32 @@ class Solver:
         if tile is not None and not tile.must:
             self._search_optimal(n_col, n_num + 1, solution)
 
-    def _add_dict(self, sets: list[Tile], tile: Tile | None) -> bool:
+    def _set_tile(self, sets: list[Tile], tile: Tile | None) -> bool:
         if not self._is_usuable(tile) or sets[tile.color.value] is not None:
             return False
         sets[tile.color.value] = tile
         tile.used = True
         return True
 
-    def unset_tile(self, sets: list[Tile | None], tile: Tile | None):
+    def _unset_tile(self, sets: list[Tile | None], tile: Tile | None):
         if tile is None:
             return
         sets[tile.color.value] = None
         tile.used = False
 
-    def _try_colors(self, color: int, number: int, solution: list):
+    def _try_colors(self, color: int, number: int, solution: list, use_users=False):
         tile = self._get_a_tile(color, number)
         sets: list[Tile | None] = [None] * 4
-        self._add_dict(sets, tile)
+        self._set_tile(sets, tile)
         count = 1
+        try_again_for_user_tile = False
         for col in reversed(range(color + 1, TILE_COLOR_MAX * TILE_BULK_MAX)):
             candidate_tile = self._get_a_tile(col, number)
-            if not self._add_dict(sets, candidate_tile):
+            if not self._set_tile(sets, candidate_tile):
+                continue
+            if use_users is False and candidate_tile.must is False:
+                self._unset_tile(sets, candidate_tile)
+                try_again_for_user_tile = True
                 continue
             count = sum(tile is not None for tile in sets)
             if count >= 4:
@@ -147,57 +147,51 @@ class Solver:
             save_tile = None
             if try_possible != color:
                 save_tile = sets[try_possible]
-                self.unset_tile(sets, save_tile)
+                self._unset_tile(sets, save_tile)
             solution.append(sets)
             self._search_optimal(color, number + 1, solution)
             solution.remove(sets)
             if save_tile is not None:
-                self._add_dict(sets, save_tile)
+                self._set_tile(sets, save_tile)
         self._mark_unused(sets)
+        if try_again_for_user_tile:
+            self._try_colors(color, number, solution, True)
 
-        #
-        #
-        #
-        #
-        # col2max = min(color+3, TILE_COLOR_MAX * TILE_BULK_MAX)
-        # for col2 in range(color + 1, col2max):
-        #     tile2 = self._get_a_tile(col2, number)
-        #     if not self._add_dict(sets, tile2):
-        #         continue
-        #
-        #     col3max = min(TILE_COLOR_MAX + color, TILE_COLOR_MAX * TILE_BULK_MAX)
-        #     for col3 in range(col2 + 1, col3max):
-        #         tile3 = self._get_a_tile(col3, number)
-        #         if not self._add_dict(sets, tile3):
-        #             continue
-        #         solution.append(sets)
-        #         self._search_optimal(color, number + 1, solution)
-        #         solution.remove(sets)
-        #
-        #         col4max = min(TILE_COLOR_MAX + color, TILE_COLOR_MAX * TILE_BULK_MAX)
-        #         for col4 in range(col3 + 1, col4max):
-        #             tile4 = self._get_a_tile(col4, number)
-        #             if not self._add_dict(sets, tile4):
-        #                 continue
-        #             solution.append(sets)
-        #             self._search_optimal(color, number + 1, solution)
-        #             solution.remove(sets)
-        #             self.unset_tile(sets, tile4)
-        #             break
-        #         self.unset_tile(sets, tile3)
-        #     self.unset_tile(sets, tile2)
-        # self.unset_tile(sets, tile)
-
-    def _try_numbers(self, color: int, number: int, solution: list):
+    def _try_numbers(self, color: int, number: int, solution: list, use_users=False):
         sets: list[Tile | None] = []
+        try_again_for_user_tile = False
         for num in range(number, TILE_NUM_MAX + 1):
             tile = self._get_a_tile(color, num)
             if not self._is_usuable(tile):
                 break
+            if tile.must is False and use_users is False:
+                try_again_for_user_tile = True
+                break
             sets.append(tile)
             tile.used = True
             if len(sets) >= 3:
+                if self._check_already_has_split_solution(sets, solution):
+                    break
                 solution.append(sets)
                 self._search_optimal(color, number + 1, solution)
                 solution.remove(sets)
         self._mark_unused(sets)
+        if try_again_for_user_tile:
+            self._try_numbers(color, number, solution, True)
+
+    # [123] [456] are same [123456] and need to remove these
+    def _check_already_has_split_solution(self, sets: list[Tile | None], solution: list[list[Tile]]):
+        if len(solution) == 0:
+            return
+        last_tile_from_solution: Tile = (solution[-1])[-1]
+        if last_tile_from_solution is None:
+            return False
+        first_tile_of_new_set = sets[0]
+        if last_tile_from_solution.color == first_tile_of_new_set.color and \
+                last_tile_from_solution.number+1 == first_tile_of_new_set.number:
+            return True
+        return False
+
+
+
+
